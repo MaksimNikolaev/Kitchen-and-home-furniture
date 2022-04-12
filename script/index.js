72dbcffaa79e61f5cd7b28205aa841cddb9bef21
Maks Nikolaev(work) <mksniklaev@gmail.com>
2022-04-12 10:47:33 +0500

    feat: add section cover and benefits

---
+++ script/index.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-cover', {
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-cover__btn-next',
+    prevEl: '.swiper-cover__btn-prev',
+  },
+
+});
