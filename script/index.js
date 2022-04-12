9d0fccb7bc8d812b3647d51f53248e566675cdb4
Maks Nikolaev(work) <mksniklaev@gmail.com>
2022-04-12 21:14:55 +0500

    feat: add section news

--- script/index.js
+++ script/index.js
@@ -37,3 +37,30 @@ const swiperPrice = new Swiper('.swiper-price', {
   slidesPerGroup: 4,
   // And if we need scrollbar
 });
+
+const swiperNews = new Swiper('.swiper-news', {
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-news__btn-next',
+    prevEl: '.swiper-news__btn-prev',
+  },
+  breakpoints: {
+    // when window width is <= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 15
+    },
+    // when window width is <= 480px
+    490: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    // when window width is <= 640px
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    }
+  },
+  slidesPerGroup: 4,
+  // And if we need scrollbar
+});
